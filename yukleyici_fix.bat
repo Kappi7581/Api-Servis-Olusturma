d15689a0aed758acf149e91f1842a4f4f2d34956
Kappi7581 <Kappi7581@outlook.com>
2025-04-05 18:49:48 +0300

    Add files via upload

---
+++ yukleyici_fix.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "%~dp0"
+echo Paketler Yukleniyor, Lutfen Bekleyiniz... By Kappi7581
+npm install express-rate-limit axios crc-32 crypto jsdom dotenv mysql
+echo.
+echo Yukleme Tamamlandi! By Kappi7581
+pause
